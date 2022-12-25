c19fa4769852ad406300741a3414466884b3f699
Lucas Hang <lucas.hhang@gmail.com>
2022-12-25 12:10:34 -0300

    feat: add initial pages and styles

---
+++ app/routes/details/contact.tsx
@@ -0,0 +1,5 @@
+function Contact() {
+  return <div>whats, email, linkedin...</div>;
+}
+
+export default Contact;
---
+++ app/routes/details/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "@remix-run/react";
+
+function DetailsIndex() {
+  return (
+    <nav>
+      <Link to="contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default DetailsIndex;
