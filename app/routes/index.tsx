f19b9ff12f71e1d95fd2a385805e0e353caa9149
Lucas Hang <lucas.hhang@gmail.com>
2022-12-30 11:28:41 -0300

    feat: add colors and fonts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,14 +12,25 @@ export default function Index() {
   let { t } = useTranslation();
 
   return (
-    <div style={{ display: "grid", placeItems: "center" }}>
-      <h1>{t("greeting")}, My name is Lucas Hang</h1>
+    <main className="container">
+      <div
+        className="content"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+      >
+        <h1>{t("greetings")}</h1>
 
-      <main>
-        <p>blablablablablabla...</p>
+        <p>
+          Comecei minha busca por conhecimento individual com cursos de
+          aprendizagem industrial, na área da informática. Hoje em dia trabalho
+          na empresa Pedidos10 e a cada dia evolúo mais corrigindo erros,
+          quebrando paradígmas e aprendendo novas tecnologias. Sei que consigo
+          trazer mais conhecimento e também aprender com meus pares. Trabalho
+          para que o produto da empresa seja mais resiliente, valorizado e
+          seguro.
+        </p>
 
         <Link to="details">details</Link>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
