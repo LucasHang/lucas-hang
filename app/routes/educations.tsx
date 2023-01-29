c25afd2d8aec672ab794f2388b95c3a715e01f58
Lucas Hang <lucas.hhang@gmail.com>
2023-01-29 10:28:53 -0300

    feat: add educations page

--- app/routes/educations.tsx
+++ app/routes/educations.tsx
@@ -1,18 +1,84 @@
 import type { LinksFunction } from "@remix-run/node";
+import { useTranslation } from "react-i18next";
+import ContentHeader, {
+  links as contentHeaderLinks,
+} from "~/components/ContentHeader";
 
 import stylesUrl from "~/styles/educations.css";
 
+const EDUCATIONS = [
+  {
+    title: "Ciência de Dados",
+    institution: "Descomplica Faculdade Digital",
+    degree: "Tecnólogo",
+    startDate: "01/2023",
+    endDate: "07/2025",
+    skillsIds: [
+      "big-data",
+      "analytics",
+      "machine-learning",
+      "artificial-intelligence",
+      "python",
+    ],
+  },
+  {
+    title: "Engenharia de Sistemas",
+    institution: "SENAI/SC",
+    degree: "Técnico em desenvolvimento e manutenção de sistemas",
+    startDate: "02/2018",
+    endDate: "07/2019",
+    skillsIds: [
+      "ruby",
+      "sql",
+      "agile",
+      "software-documentation",
+      "java",
+      "network",
+    ],
+  },
+];
+
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
+  return [...contentHeaderLinks(), { rel: "stylesheet", href: stylesUrl }];
 };
 
 export default function Educations() {
+  const { t } = useTranslation();
+
   return (
-    <main className="container">
+    <div className="educations-container">
+      <ContentHeader title={t("question_education")} />
+
       <div className="content">
-        <h1>Educations page</h1>
-        <p>I'm sorry, this page is in development process...</p>
+        {EDUCATIONS.map((education, index) => {
+          let datesInterval = `${education.startDate} - ${education.endDate}`;
+
+          //   const skills = getHardSkillsByIds(education.skillsIds);
+
+          return (
+            <div key={index} className="education">
+              <div className="education-dates">
+                <hr />
+                <span>{datesInterval}</span>
+                <hr />
+              </div>
+
+              <h5>{education.title}</h5>
+              <span>{education.institution}</span>
+
+              <br />
+
+              <small>{education.degree}</small>
+
+              {Boolean(education.skillsIds.length) && (
+                <p>
+                  <strong>Habilidades:</strong> {education.skillsIds.join(", ")}
+                </p>
+              )}
+            </div>
+          );
+        })}
       </div>
-    </main>
+    </div>
   );
 }
